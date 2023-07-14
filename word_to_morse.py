dfd5ad49c52294b0fecef6f490f2e96e2eacbbb2
MagnusPala <119525794+MagnusPala@users.noreply.github.com>
2023-07-14 18:47:06 +0300

    Update word_to_morse.py

--- word_to_morse.py
+++ word_to_morse.py
@@ -1,48 +1,20 @@
-#this code outputs international morse code(ITU) out of a given word
-
+#this code outputs international morse code of a given text
 import re
-word='hello world'
-word=word.lower()
-def encode(x):
-    if x == 'a':return ".-"
-    elif x == 'b':return "-..."
-    elif x == 'c':return "-.-."
-    elif x == 'd':return "-.."
-    elif x == 'e':return "."
-    elif x == 'f':return "..-."
-    elif x == 'g':return "--."
-    elif x == 'h':return "...."
-    elif x == 'i':return ".."
-    elif x == 'j':return ".---"
-    elif x == 'k':return "-.-"
-    elif x == 'l':return ".-.."
-    elif x == 'm':return "--"
-    elif x == 'n':return "-."
-    elif x == 'o':return "---"
-    elif x == 'p':return ".--."
-    elif x == 'q':return "--.-"
-    elif x == 'r':return ".-."
-    elif x == 's':return "..."
-    elif x == 't':return "-"
-    elif x == 'u':return "..-"
-    elif x == 'v':return "...-"
-    elif x == 'w':return ".--"
-    elif x == 'x':return "-..-"
-    elif x == 'y':return "-.--"
-    elif x == 'z':return "--.."
-    elif x == '1':return ".----"
-    elif x == '2':return "..---"
-    elif x == '3':return "...--"
-    elif x == '4':return "....-"
-    elif x == '5':return "....."
-    elif x == '6':return "-...."
-    elif x == '7':return "--..."
-    elif x == '8':return "---.."
-    elif x == '9':return "----."
-    elif x == '0':return "-----"
-    return '/'
+text='hello world'
+text=text.upper()
+text=re.sub('_','-',text)
+DICT = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+            'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-',
+            'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-',
+            'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--',
+            'X': '-..-', 'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--',
+            '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+            '0': '-----', ', ': '--..--', '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-',
+            '(': '-.--.', ')': '-.--.-'}
 output=''
-for x in word:
-    output+=encode(x)+' '
-output=re.sub(' / ','/',output)[:-1]
-print(output)
+for x in text:
+    if x==' ':
+        output+='/ '# / for seperating words
+        continue
+    output+=DICT[x]+' '
+output=output[:-1]#debug
